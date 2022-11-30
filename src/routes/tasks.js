515b9c6d15e039f5f459aed2daa31ba2705e0f9c
Vicky Soni <murlimanoharsoni4@gmail.com>
2022-11-30 21:26:45 +0530

    added bulk add feature

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -37,15 +37,27 @@ router.get("/:id", async (req, res) => {
 
 router.post("/", async (req, res) => {
     try {
-        const { title } = req.body;
-        if (!title) {
+        const { title, tasks } = req.body;
+        if (!title && !tasks) {
             return res.status(404).json({
-                error: "Provide a valid title",
+                error: "Provide a valid title or tasks array to add",
             });
         }
 
-        const result = await Task.create({ title });
-        res.status(201).json({ id: result._id });
+        if (title) {
+            const result = await Task.create({ title });
+            return res.status(201).json({ id: result._id });
+        }
+
+        if (tasks) {
+            const result = await Task.create(tasks);
+            const responseArr = result.map(taskObj => {
+                return {id: taskObj._id}
+            });
+            return res.status(201).json({
+                tasks: responseArr
+            });
+        }
     } catch (err) {
         res.status(500).json({
             error: "Some error ocurred while creating the task",
@@ -76,5 +88,43 @@ router.delete("/:id", async (req, res) => {
     }
 });
 
+router.put("/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { title, is_completed } = req.body;
+
+        if (!id) {
+            return res.status(404).json({ error: "Provide valid id" });
+        }
+
+        if (title === undefined || is_completed === undefined) {
+            return res.status(404).json({
+                error: "Provide valid title and is_completed property",
+            });
+        }
+
+        const result = await Task.findById(id);
+
+        //console.log(result, "deleted");
+        if (!result) {
+            return res
+                .status(404)
+                .json({ error: "There is not task at that id" });
+        }
+
+        const newTask = await Task.findByIdAndUpdate(
+            id,
+            { $set: { title, is_completed } },
+            { new: true }
+        );
+        console.log(newTask, "updated task");
+        res.status(204).end();
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            error: "Some error ocurred while creating the task",
+        });
+    }
+});
 
 module.exports = router;
